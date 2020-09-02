68af63926ea25b9079da6d787e5e1a5108e30c4c
Luis Zamora <lzamora@espol.edu.ec>
2020-09-02 01:01:19 -0500

    Updates

---
+++ precache-manifest.443345f145e5064d0ad226a35a464b04.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c5ddbf0c1276086319de7a4c1b018f2",
+    "url": "/chatbot/index.html"
+  },
+  {
+    "revision": "86a6fae4428bf35ebdab",
+    "url": "/chatbot/static/css/main.92c34630.chunk.css"
+  },
+  {
+    "revision": "944583c5713bd5604045",
+    "url": "/chatbot/static/js/2.81dd9a71.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/chatbot/static/js/2.81dd9a71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86a6fae4428bf35ebdab",
+    "url": "/chatbot/static/js/main.e1f4ed4e.chunk.js"
+  },
+  {
+    "revision": "046c329087ca1c6799b2",
+    "url": "/chatbot/static/js/runtime-main.56d9e5c4.js"
+  },
+  {
+    "revision": "7257a1a94be550133f3233fd792ee724",
+    "url": "/chatbot/static/media/avatar.7257a1a9.jpg"
+  },
+  {
+    "revision": "d01aa8a4015581a8f76f02877c73243b",
+    "url": "/chatbot/static/media/bolon.d01aa8a4.jpg"
+  },
+  {
+    "revision": "f3f80fadb99278ac6e4f114de30768c0",
+    "url": "/chatbot/static/media/encebollado.f3f80fad.jpeg"
+  },
+  {
+    "revision": "089c4f4e55f158a69d0364c6b6339f37",
+    "url": "/chatbot/static/media/eva.089c4f4e.jpg"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/chatbot/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/chatbot/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/chatbot/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/chatbot/static/media/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "78007ab829fbcb5d25c297e398cf946b",
+    "url": "/chatbot/static/media/restaurante.78007ab8.jpg"
+  },
+  {
+    "revision": "f217f346c88e5fb2720ca7c28827ff72",
+    "url": "/chatbot/static/media/ryan.f217f346.jpg"
+  },
+  {
+    "revision": "345556df30ac699156933c06ba382ea1",
+    "url": "/chatbot/static/media/yapingacho.345556df.jpg"
+  }
+]);
